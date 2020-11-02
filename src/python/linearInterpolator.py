3be12201f0067c2256bd8718aefa7b7866a603bf
Leonardo Fontanelli <l.fontanelli2@studenti.unipi.it>
2020-11-02 17:05:31 +0100

    Trovato errore in formule Python, corretto e rigenerati output

--- src/python/linearInterpolator.py
+++ src/python/linearInterpolator.py
@@ -8,16 +8,34 @@ y_coords = []
 
 L = 4  # interpolation factor
 T = 1  # period time
+j = 0
 i = 0
 
 for i in range(0, len(input_points) - 1):
-    k = 0
-    x_coords.append(T*(i-1))
-    while k < L:
-        u = k/L
-        output.append(int((input_points[i + 1] - input_points[i]) * u + input_points[i])) # calculate the interpolation
-        y_coords.append((i-1)*T+u*T)  # calculate the coordinates of the interpolated points
-        k = k+1
+    x_coords.append(T * (i - 1))
+    k = 0  # first point
+    output.append(input_points[i])
+    y_coords.append(((i - 1) * T + (k / L) * T))  # calculate the coordinates of the interpolated points
+    print("Element ", j, ": ", input_points[i])
+    j += 1
+    k = 1  # second point
+    element = np.math.floor(input_points[i + 1] / 4) + np.math.floor(input_points[i] / 2) + np.math.floor(input_points[i] / 4)
+    output.append(element)
+    y_coords.append(((i - 1) * T + (k / L) * T))  # calculate the coordinates of the interpolated points
+    print("Element ", j, ": ", element)
+    j += 1
+    k = 2  # third point
+    element = np.math.floor(input_points[i+1] / 2) + np.math.floor(input_points[i] / 2)
+    output.append(element)
+    y_coords.append(((i - 1) * T + (k / L) * T))  # calculate the coordinates of the interpolated points
+    print("Element ", j, ": ", element)
+    j += 1
+    k = 3  # fourth point
+    element = np.math.floor(input_points[i] / 4) + np.math.floor(input_points[i+1] / 4) + np.math.floor(input_points[i+1] / 2)
+    output.append(element)
+    y_coords.append(((i - 1) * T + (k / L) * T))  # calculate the coordinates of the interpolated points
+    print("Element ", j, ": ", element)
+    j += 1
 x_coords.append(T*i)  # calculate the coordinates of the input points
 
 plt.plot(x_coords, input_points, marker='o')
